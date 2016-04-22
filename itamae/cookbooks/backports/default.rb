e7bf3d89048e9b77db1892a5b2e6881d4a67b82d
NoNameA 774 <nonamea.774@gmail.com>
2016-04-23 02:58:55 +0900

    backportsの設定

---
+++ itamae/cookbooks/backports/default.rb
@@ -0,0 +1,19 @@
+file "/etc/apt/sources.list.d/backports.list" do
+  action :create
+  mode "0644"
+  owner "root"
+  group "root"
+  content "deb http://ftp.debian.org/debian #{node[:debian_release]}-backports main\n"
+end
+
+file "/etc/apt/preferences.d/backports" do
+  action :create
+  mode "0644"
+  owner "root"
+  group "root"
+  content <<-EOS
+Package: *
+  Pin: release a=#{node[:debian_release]}-backports
+  Pin-Priority: 600
+EOS
+end
--- itamae/hosts/sakura.nna774.net/default.rb
+++ itamae/hosts/sakura.nna774.net/default.rb
@@ -1,6 +1,8 @@
 node.reverse_merge!(host: "sakura.nna774.net")
+node.reverse_merge!(debian_release: 'wheezy')
 
 include_role 'base'
 
 include_cookbook 'utils'
 include_cookbook 'hostname'
+include_cookbook 'backports'
