9615dbf402d94ee4903d175bcdb184aac2139a07
NoNameA 774 <nonamea.774@gmail.com>
2016-04-23 02:31:32 +0900

    nano忘れてた

--- itamae/cookbooks/utils/default.rb
+++ itamae/cookbooks/utils/default.rb
@@ -1,4 +1,4 @@
-packages = %w(zsh tmux git less nkf dnsutils wget build-essential command-not-found w3m bash-completion man-db htop)
+packages = %w(zsh tmux git less nkf dnsutils wget build-essential command-not-found w3m bash-completion man-db htop nano)
 packages.each do |package|
   package package do
     action :install
