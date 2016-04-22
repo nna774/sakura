be9853ad59950cce6903b90fb532ce85a50d427b
NoNameA 774 <nonamea.774@gmail.com>
2016-04-23 03:02:16 +0900

    これぐらいならcontentのほうが読みやすい

--- itamae/cookbooks/hostname/default.rb
+++ itamae/cookbooks/hostname/default.rb
@@ -1,7 +1,7 @@
-template "/etc/hostname" do
+file "/etc/hostname" do
   action :create
   mode "0644"
   owner "root"
   group "root"
-  variables(host: node[:host])
+  content node[:host] << "\n"
 end
