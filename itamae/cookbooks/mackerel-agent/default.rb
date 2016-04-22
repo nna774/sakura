fb6312364773613805cbae9f530d97db88c12168
NoNameA 774 <nonamea.774@gmail.com>
2016-04-23 04:52:25 +0900

    add mackerel-agent settings

---
+++ itamae/cookbooks/mackerel-agent/default.rb
@@ -0,0 +1,18 @@
+execute "add mackerel-agent apt repo" do
+  command "curl -fsSL https://mackerel.io/assets/files/scripts/setup-apt.sh | sh"
+  not_if "test -e /etc/apt/sources.list.d/mackerel.list"
+end
+
+package 'mackerel-agent' do
+  action :install
+end
+
+api_key = node[:secrets][:"mackerel-apikey"]
+execute "add api key" do
+  command "echo 'apikey = \"#{api_key}\"' >> /etc/mackerel-agent/mackerel-agent.conf"
+  not_if "grep #{api_key} /etc/mackerel-agent/mackerel-agent.conf"
+end
+
+service "mackerel-agent" do
+  action [ :start, :enable ]
+end
