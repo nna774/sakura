ea99f049ce1ecd09cc50a2ed629b5f0ff22a665d
NoNameA 774 <nonamea.774@gmail.com>
2016-05-18 20:54:49 +0900

    存在判定が難しいのでとりあえずスキップ……(必要となったらコメントアウトするなりしてくれ……)。

--- itamae/cookbooks/mackerel-agent/plugins.rb
+++ itamae/cookbooks/mackerel-agent/plugins.rb
@@ -2,5 +2,6 @@ version = node[:"mackerel-agent"][:"plugin-version"]
 
 package "https://github.com/mackerelio/mackerel-agent-plugins/releases/download/v#{version}/mackerel-agent-plugins-#{version}-1.x86_64.rpm" do
   action :install
-  not_if "VER=$(yum list mackerel-agent-plugins | grep mackerel-agent-plugins | head -n1 | cut -f2 -d' '); [ \"x$VER\" == \"x#{version}-1\" ]"
+  not_if "true"
+#  not_if "VER=$(yum list mackerel-agent-plugins | grep mackerel-agent-plugins | head -n1 | cut -f2 -d' '); [ \"x$VER\" == \"x#{version}-1\" ]"
 end
