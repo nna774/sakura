5d6701d626f458c0f8da133914d785640ab6597b
NoNameA 774 <nonamea.774@gmail.com>
2016-06-01 01:08:11 +0900

    とりあえずテンプレートに

--- itamae/cookbooks/fail2ban/default.rb
+++ itamae/cookbooks/fail2ban/default.rb
@@ -17,12 +17,19 @@ files.each do |file|
 end
 
 fail2ban_slack_wh_uri = node[:secrets][:"sakura-slack-wh-uri"]
+slack_icon_uris = %w(
+https://pbs.twimg.com/media/BhFKgEDCQAA7ZGK.jpg
+https://pbs.twimg.com/media/BeWJQSuCIAAN4ls.jpg
+https://pbs.twimg.com/media/BdsvbA-CYAAjw3_.jpg
+https://pbs.twimg.com/media/BcbTiKDCMAEwKik.jpg
+https://pbs.twimg.com/media/BhEDeAkCMAAXfnM.jpg
+).join(' ')
 template "/usr/local/bin/fail2ban_slack.sh" do
   action :create
   mode "0755"
   owner "root"
   group "root"
-  variables(fail2ban_slack_wh_uri: fail2ban_slack_wh_uri)
+  variables(fail2ban_slack_wh_uri: fail2ban_slack_wh_uri, slack_icon_uris: slack_icon_uris)
 end
 
 service "fail2ban" do
--- itamae/cookbooks/fail2ban/templates/usr/local/bin/fail2ban_slack.sh
+++ itamae/cookbooks/fail2ban/templates/usr/local/bin/fail2ban_slack.sh
@@ -3,7 +3,7 @@
 IP=$1
 NAME=$2
 HOST=`dig -x $IP PTR | grep -v -e ";" -e "^$" | head -n1 | cut -f5`
-URIS=(https://pbs.twimg.com/media/BhFKgEDCQAA7ZGK.jpg https://pbs.twimg.com/media/BeWJQSuCIAAN4ls.jpg https://pbs.twimg.com/media/BdsvbA-CYAAjw3_.jpg https://pbs.twimg.com/media/BcbTiKDCMAEwKik.jpg https://pbs.twimg.com/media/BhEDeAkCMAAXfnM.jpg)
+URIS=(<%= @slack_icon_uris %>)
 URI=${URIS[ $RANDOM % ${#URIS[@]} ]}
 
 curl -X POST --data-urlencode "payload={\"channel\": \"#nona-kanshi\",\
