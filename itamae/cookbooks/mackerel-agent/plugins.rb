0f53a09121156d2526e0746007178f27ad47ddc0
NoNameA 774 <nonamea.774@gmail.com>
2016-06-18 01:09:27 +0900

    install mackerel-check-plugins

--- itamae/cookbooks/mackerel-agent/plugins.rb
+++ itamae/cookbooks/mackerel-agent/plugins.rb
@@ -1,7 +1,13 @@
 version = node[:"mackerel-agent"][:"plugin-version"]
 
+//  /etc/yum.repos.d/mackerel.repo を置いて yum install したらいいんでは。
+
 package "https://github.com/mackerelio/mackerel-agent-plugins/releases/download/v#{version}/mackerel-agent-plugins-#{version}-1.x86_64.rpm" do
   action :install
   not_if "true"
 #  not_if "VER=$(yum list mackerel-agent-plugins | grep mackerel-agent-plugins | head -n1 | cut -f2 -d' '); [ \"x$VER\" == \"x#{version}-1\" ]"
+end
+
+package "mackerel-check-plugins" do
+  action :install
 end
