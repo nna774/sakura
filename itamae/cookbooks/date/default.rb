a4b8b91dcf57e387ec8e22e3e09bc7003ea61fc3
NoNameA 774 <nonamea.774@gmail.com>
2016-06-20 20:16:35 +0900

    ntpdがデフォルトだと死ぬので、起動オプションを変更

--- itamae/cookbooks/date/default.rb
+++ itamae/cookbooks/date/default.rb
@@ -8,6 +8,12 @@ file "/etc/timezone" do
   content "Asia/Tokyo\n"
 end
 
+remote_file "/etc/systemd/system/ntpd.service" do
+  owner 'root'
+  group 'root'
+  mode '644'
+end
+
 ntp = 'ntp'
 package ntp do
   action :install
