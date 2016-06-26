e97629b8edaf6bae342440bc73247b6d23185794
NoNameA 774 <nonamea.774@gmail.com>
2016-06-27 03:10:07 +0900

    zshは使わない！

--- itamae/cookbooks/utils/default.rb
+++ itamae/cookbooks/utils/default.rb
@@ -2,7 +2,7 @@ package 'epel-release' do
   action :install
 end
 
-packages = %w(zsh tmux git less wget gcc gcc-c++ make w3m bash-completion man-db htop nano which file nmap bind-utils)
+packages = %w(tmux git less wget gcc gcc-c++ make w3m bash-completion man-db htop nano which file nmap bind-utils)
 packages.each do |package|
   package package do
     action :install
