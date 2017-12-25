f65c170c6fa1d95061ca6822b7ab3826276dc45a
NoNameA 774 <nonamea.774@gmail.com>
2017-12-26 05:36:29 +0900

    いつもの

--- itamae/hosts/sakura.nna774.net/default.rb
+++ itamae/hosts/sakura.nna774.net/default.rb
@@ -20,7 +20,7 @@ node.reverse_merge!(utils:{ packages: %w(epel-release gcc-c++ bind-utils)})
 
 node.reverse_merge!(nona: {
                       authorized_keys: [
-                        "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmIet5NxOTogk52tPSIe8QwIR9FEgnNi8euSfH1LUESIn9msJ6tGn0boCTS4SS9Al7EyE03PYq2DRgpia0u/puHFFjnrEhw/rGGeduIXBrF/1jlavqxtZSFWv7h/Ll4vd9EfW5aW9fIkBHm9s+tPkCYUuefN9nimtTC2oPntyA7C9eg8/D/Hvhnp9aBQSUYJYZYDVw4cIHhwRcvO98cg28Cp/WI3Cq6ObGVIUjrqeQxqX0APyiw+n6ulDhCqGJxlkcih02TjNsrWE37xQHGEscygOF0s8j80oIm2lXiEE8OyK6s/XxsEx4mDp2vlpJGQR/2Hx9Cobc2QssNWqaanM5w== ikaduchi-20151007",
+                        "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAEYIrOpHsdw23+Fn9aA9ISvIlrx2aUhnZqPHHM28TrxcbT8EWHG5xC3Ge6CNJ13sENPO23xa/QHs1VS2R2DQsMdOgB5oUYVMTSxmoQVXptqXcJkgLtKmWxZzvZPA2fsdfG/yvdyinwZOAx6HJVvoPM4CLXhkVWQuOHdhC61GkIeWWXyqQ== ikaduchi-20170510",
                         "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuSWaN5ZkzTLJDgliJbctzQJZhvwUPoeAjALmS2bkhymhcCFNXVU6HvsBG9LSqFgl1ghZV7Jx/oEjicaepnSC71zyr/TcIckSrp4HjNVoFqjuwUdZmC9raAvBYnoa23uvmzIQfnfWgF9fh2mGQ6pkQICvljF/nyNif2p+HN5rWSYn1s52+Bn7Sqmba1Ncxm9F/Q58l0BICRjt4QdbXSRrqGWLtPbNv+wIDdLoqrojHUcrV4Yg1J3QxdH5dpChFhE5PLRrK0Ldz9SEYg6oE7cMuew9YPv3teRLiMNwwGAUUEA8q99ur82kUs4GiGJFMOzlIwp/g+sR79TbKajhPG3Lvw== nona@tsukumothan-2016-11-14",
                         'permitopen="localhost:25",command="/bin/true",no-X11-forwarding,no-agent-forwarding,no-pty ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDE6U/7+MlTQt6WmimeiPe1TeMlcBNrZqg+wolEPUhqeXjEE7wtbQpesHh5hKaSahQ6fpZp+rpbw3BTyp2J9EqXkdsVKXGWULtEH/we2K++pViacexVny3FvLJHk6a3kcDImAtsojnwW08cTQPeqghkSRbeczuUQXByrF6qf1H8W3fCMSyzpgkds1/rAdqmktiRc+N/De0ken/xn3j+b8O0hL7BWhPkavh9CNuTbgS9Y8zRZ7IwP268cIbpTuYpdldjzrNptFMfOqIRajFEaP1e34pRLq2RtMGKeB0o3qbMMXkw5R5Saw+M9X14JKYqvKfnuLRyYondn68+LHNRivi5 root@ushio.nna774.net', # ssh port forward settings between ushio, for mail
                       ],
@@ -46,7 +46,7 @@ include_cookbook 'mongodb'
   '/etc/systemd/system/certbot-renew.service',
   '/etc/systemd/system/certbot-renew.timer'
 ].each do |f|
-  file f do
+  remote_file f do
     mode "0644"
     owner "root"
     group "root"
